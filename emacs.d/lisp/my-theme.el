6e3e835c28bbd50591890921bc26ca585c3d4ca3
Steven Engelhardt <sengelha@gmail.com>
2026-02-23 18:35:36 -0600

    Fix emacs themeing for emacs daemon mode

--- emacs.d/lisp/my-theme.el
+++ emacs.d/lisp/my-theme.el
@@ -2,23 +2,27 @@
   "Return existing font which first match."
   (cl-find-if (lambda (f) (find-font (font-spec :name f))) fonts))
 
-(let* ((fixed-font (font-candidate "BlexMono Nerd Font" "Inconsolata Nerd Font" "Inconsolata" "Fira Code" "Consolas" "Menlo" "DejaVu Sans Mono"))
-       (variable-font (font-candidate "SF Pro" "Segoe UI" "Ubuntu Sans" "Lucida Grande" "Helvetica Neue" "Helvetica" "Arial"))
-       (fixed-font-height 140)
-       (variable-font-height 140))
-  (set-face-attribute 'default nil
-                      :family fixed-font
-                      :height fixed-font-height)
-  (set-face-attribute 'fixed-pitch nil
-                      :family fixed-font
-                      :height fixed-font-height)
-  (set-face-attribute 'variable-pitch nil
-                      :family variable-font
-                      :height variable-font-height))
+(defun my-frame-setup (frame)
+  "Custom setup for a newly created FRAME."
+  (select-frame frame)
+  (set-frame-size frame 180 80)
+  (let* ((fixed-font (font-candidate "BlexMono Nerd Font" "Inconsolata Nerd Font" "Inconsolata" "Fira Code" "Consolas" "Menlo" "DejaVu Sans Mono"))
+         (variable-font (font-candidate "SF Pro" "Segoe UI" "Ubuntu Sans" "Lucida Grande" "Helvetica Neue" "Helvetica" "Arial"))
+         (fixed-font-height 140)
+         (variable-font-height 140))
+    (set-face-attribute 'default nil
+                        :family fixed-font
+                        :height fixed-font-height)
+    (set-face-attribute 'fixed-pitch nil
+                        :family fixed-font
+                        :height fixed-font-height)
+    (set-face-attribute 'variable-pitch nil
+                        :family variable-font
+                        :height variable-font-height)))
 
-;; Set window size
-(if (window-system)
-    (set-frame-size (selected-frame) 180 80))
+(add-hook 'after-make-frame-functions #'my-frame-setup)
+(unless (daemonp)
+  (my-frame-setup (selected-frame)))
 
 ;; doom-themes: Use Doom color themes
 (use-package doom-themes
